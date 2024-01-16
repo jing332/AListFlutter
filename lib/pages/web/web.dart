968fdce926dab1a1ec7a5d252b74d5b4cf76b04a
Jing <42014615+jing332@users.noreply.github.com>
2024-01-16 09:18:51 +0800

    Web page download file

--- lib/pages/web/web.dart
+++ lib/pages/web/web.dart
@@ -1,5 +1,7 @@
 import 'package:alist_flutter/generated_api.dart';
+import 'package:android_intent_plus/android_intent.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
 
@@ -58,11 +60,16 @@ class _WebScreenState extends State<WebScreen> {
                     return NavigationActionPolicy.ALLOW;
                   }
                   Get.showSnackbar(GetSnackBar(
-                      message:
-                          "shouldOverrideUrlLoading ${navigationAction.request.url}",
+                      message: "是否跳转到其他应用？",
                       duration: const Duration(seconds: 3),
                       mainButton: TextButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          final intent = AndroidIntent(
+                              action: "action_view",
+                              data: navigationAction.request.url!.toString());
+
+                          intent.launchChooser("选择应用");
+                        },
                         child: const Text('前往'),
                       )));
 
@@ -70,12 +77,39 @@ class _WebScreenState extends State<WebScreen> {
                 },
                 onDownloadStartRequest: (controller, url) async {
                   Get.showSnackbar(GetSnackBar(
-                      message: "是否下载文件？ ${url.contentDisposition}",
-                      duration: const Duration(seconds: 3),
-                      mainButton: TextButton(
-                        onPressed: () {},
+                    title: "是否下载文件？",
+                    message: url.suggestedFilename ??
+                        url.contentDisposition ??
+                        url.toString(),
+                    duration: const Duration(seconds: 3),
+                    mainButton: Column(children: [
+                      TextButton(
+                        onPressed: () {
+                          final intent = AndroidIntent(
+                              action: "action_view", data: url.url.toString());
+                          intent.launchChooser("选择应用");
+                        },
+                        child: const Text('选择应用下载'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          final intent = AndroidIntent(
+                              action: "action_view", data: url.url.toString());
+                          intent.launch();
+                        },
                         child: const Text('下载'),
-                      )));
+                      ),
+                    ]),
+                    onTap: (_) {
+                      Clipboard.setData(
+                          ClipboardData(text: url.url.toString()));
+                      Get.closeCurrentSnackbar();
+                      Get.showSnackbar(const GetSnackBar(
+                        message: "下载链接已复制到剪贴板",
+                        duration: Duration(seconds: 1),
+                      ));
+                    },
+                  ));
                 },
                 onLoadStop:
                     (InAppWebViewController controller, Uri? url) async {
@@ -117,6 +151,9 @@ class _WebScreenState extends State<WebScreen> {
                     if (_webViewController != null) {
                       _webViewController!.reload();
                     }
+                    _webViewController!.loadUrl(
+                        urlRequest:
+                            URLRequest(url: WebUri("http://coolapk.com")));
                   },
                 ),
               ],
