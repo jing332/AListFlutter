9900c86fb21336c6a8a0fd85df7ec68024d08394
asogii <124483630+asogii@users.noreply.github.com>
2024-05-28 22:34:39 +0800

    Update init_alist.sh

--- alist-lib/scripts/init_alist.sh
+++ alist-lib/scripts/init_alist.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-GIT_REPO="https://github.com/alist-org/alist.git"
+GIT_REPO="https://github.com/asogii/alist.git"
 TAG_NAME=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags $GIT_REPO | tail --lines=1 | cut --delimiter='/' --fields=3)
 
 echo "AList - ${TAG_NAME}"
 rm -rf ./src
 unset GIT_WORK_TREE
-git clone --branch "$TAG_NAME" https://github.com/alist-org/alist.git ./src
+git clone --branch "$TAG_NAME" https://github.com/asogii/alist.git ./src
 rm -rf ./src/.git
 
 mv -f ./src/* ../
