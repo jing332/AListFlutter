d497a87d50a660730c7203c2d1b1ca97960a8268
asogii <124483630+asogii@users.noreply.github.com>
2024-05-28 22:37:31 +0800

    Update init_web.sh

--- alist-lib/scripts/init_web.sh
+++ alist-lib/scripts/init_web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L https://github.com/alist-org/alist-web/releases/latest/download/dist.tar.gz -o dist.tar.gz
+curl -L https://github.com/asogii/alist-web/releases/latest/download/dist.tar.gz -o dist.tar.gz
 tar -zxvf dist.tar.gz
 rm -rf ../public/dist
 mv -f dist ../public
