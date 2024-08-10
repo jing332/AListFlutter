c44951e2235c75f4ed32e1d5fc64bc7331db12e0
Jing <42014615+jing332@users.noreply.github.com>
2024-08-10 20:00:56 +0800

    Update gobind.sh
    Change android api to default

--- alist-lib/scripts/gobind.sh
+++ alist-lib/scripts/gobind.sh
@@ -2,9 +2,9 @@
 
 cd ../alistlib || exit
 if [ "$1" == "debug" ]; then
-  gomobile bind -ldflags "-s -w" -v -androidapi 19 -target="android/arm64"
+  gomobile bind -ldflags "-s -w" -v -target="android/arm64"
 else
-  gomobile bind -ldflags "-s -w" -v -androidapi 19
+  gomobile bind -ldflags "-s -w" -v
 fi
 
 echo "Moving aar and jar files to android/app/libs"
