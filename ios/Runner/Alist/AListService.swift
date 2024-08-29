03729abf77627f188898c3c39b659ff7b2dcfabc
apple <toutoumu@126.com>
2024-08-29 11:47:32 +0800

    添加ios相关代码

---
+++ ios/Runner/Alist/AListService.swift
@@ -0,0 +1,15 @@
+//
+//  AListService.swift
+//  Runner
+//
+//  Created by apple on 2024/8/18.
+//
+
+import Foundation
+
+class AListService:NSObject{
+    static var instance = AListService()
+
+    private override init() {}
+    
+}
